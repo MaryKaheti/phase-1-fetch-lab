2e9a5288d2c679b4d291f78cf3a7539d8a0c241e
MaryKaheti <kahetikihara@gmail.com>
2023-03-26 14:22:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch("https://anapioficeandfire.com/api/books")
+  .then((res) => res.json())
+  .then(data => renderBooks(data));
   
 }
 
@@ -8,7 +11,8 @@ function renderBooks(books) {
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
-    main.appendChild(h2);
+    main.appendChild(h2)
+    fetchBooks();
   });
 }
 
